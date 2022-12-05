a0e991807340015b8bd880fb079cf2ad665ac5f8
ASuzukikkk <94042768+ASuzukikkk@users.noreply.github.com>
2022-12-05 16:35:47 +0900

    Update kotetsu044.py

--- kotetsu044.py
+++ kotetsu044.py
@@ -10,4 +10,5 @@ class AI(object):
             x = random.randint(0, board.N+1)
             y = random.randint(0, board.N+1)
             if board.put_and_reverse(x, y, color, reverse=False) > 0:
-                return (x, y
+                return (x, y)
+            
