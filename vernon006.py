89691db25efd24f440d12e9bddde0dfd3ea58df0
Shiho <66916042+artms007@users.noreply.github.com>
2022-12-05 16:15:58 +0900

    Create vernon006.py

---
+++ vernon006.py
@@ -0,0 +1,11 @@
+from .reversi import *
+
+class AI(GameAI):
+    def name(self):
+        return 'Jesse006'
+
+    def play(self, board, color):
+        for y in range(board.N):
+            for x in range(board.N):
+                if board.put_and_reverse(x, y, color, reverse=False) > 0:
+                    return (x, y)
