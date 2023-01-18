46978ff15beec6a3161558b1dac143f91509d0e3
Ryoko619 <82200409+Ryoko619@users.noreply.github.com>
2023-01-19 00:15:24 +0900

    Update ryoko093.py

--- ryoko093.py
+++ ryoko093.py
@@ -2,12 +2,23 @@ from reversi2022.reversi import *
 import random
 
 class rAI(object):
-  def name(self):
-    return 'ryoko'
-    
-  def play(self,board,color):
-    while True:
-      x = random.randint(0,board.N+1)
-      y = random.randint(0,board.N+1)
-      if board .put_and_reverse(x,y,color,reverse=False)>0:
-        return(x,y)
+    def name(self):
+        return 'ryoko'
+
+    def play(self, board, color):
+        point = [[120,-20,20,5,5,20,-20,120],
+        [-20,-40,-5,-5,-5,-5,-40,-20],
+        [20,-5,15,3,3,15,-5,20],
+        [5,-5,3,3,3,3,-5,5],
+        [5,-5,3,3,3,3,-5,5],
+        [20,-5,15,3,3,15,-5,20],
+        [-20,-40,-5,-5,-5,-5,-40,-20],
+        [120,-20,20,5,5,20,-20,120]]
+        stone_score = []
+        stone_location = []
+        for y in range(board.N):
+          for x in range(board.N):
+            if board.put_and_reverse(x, y, color, reverse=False) > 0:
+              stone_score.append(point[y][x])
+              stone_location.append((x, y))
+        return stone_location[stone_score.index(max(stone_score))]
