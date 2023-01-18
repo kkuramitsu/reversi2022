ba26fb52f45aae31e730c2396d8bbd569394acf2
0087km <82272036+0087km@users.noreply.github.com>
2023-01-19 00:00:57 +0900

    Update mm030.py

--- mm030.py
+++ mm030.py
@@ -1,11 +1,17 @@
 from reversi2022.reversi import*
 import random
 
-class AI(object):
+class mmAI(object):
     def name(self):
         return 'mm030'
 
     def play(self, board, color):
+      evalution_board = [[10, -5, 5, 5, -5, 10],
+                        [-5, -8, 1, 1, -8, -5],
+                        [5, 1, 0, 0, 1, 5],
+                        [5, 1, 0, 0, 1, 5],
+                        [-5, -8, 1, 1, -8, -5],
+                        [10, -5, 5, 5, -5, 10]]
       while True:
         x = random.randint(0, board.N+1)
         y = random.randint(0, board.N+1)
