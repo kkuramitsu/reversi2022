fcf705b7007a9cbb63eb20827ac0ecd7654eb360
chikochiko76 <94045178+chikochiko76@users.noreply.github.com>
2023-01-19 02:32:48 +0900

    Update mofumofu045.py

--- mofumofu045.py
+++ mofumofu045.py
@@ -4,16 +4,51 @@ import  random
 class AI(object):
     def name(self):
         return 'mofumofu045'
+        
+def play(self,board, color):	
+	a = []
 
-    def play(self, board, color):
-      list = [20,-10,0,0,-10,20
-               -10,-5,-2,-2,-5,-10,
-                0,-2,0,0,-2,0,
-                0,-2,0,0,-2,0,
-               -10,-5,-2,-2,-5,-10,
-                20,-10,0,0,-10,20]
-      while True:
-            x=random.randint(0, board.N+1)
-            y=random.randint(0, board.N+1)
-            if board.put_and_reverse(x,y,color,reverse=False)>0:
-                return (x,y) 
+	rank1=[0,5,30,35]
+	rank2=[1,4,6,11,24,29,31,34]
+	rank3=[2,3,12,17,18,23,32,33]
+	rank4=[7,10,25,28]
+	rank5=[8,9,13,16,19,22,26,27]
+	rank6=[14,15,20,21]
+
+	for position in rank1:
+		if put_and_reverse(board, position, color):
+			a.append(position)
+			if len(a) > 0:
+				return random.choice(a)
+    
+	for position in rank3:
+		if put_and_reverse(board, position, color):
+			a.append(position)
+			if len(a) > 0:
+				return random.choice(a)
+    
+	for position in rank5:
+		if put_and_reverse(board, position, color):
+			a.append(position)
+			if len(a) > 0:
+				return random.choice(a)
+    
+	for position in rank6:
+		if put_and_reverse(board, position, color):
+			a.append(position)
+			if len(a) > 0:
+				return random.choice(a)
+    
+	for position in rank2:
+		if put_and_reverse(board, position, color):
+			a.append(position)
+			if len(a) > 0:
+				return random.choice(a)
+    
+	for position in rank4:
+		if put_and_reverse(board, position, color):
+			a.append(position)
+			if len(a) > 0:
+				return random.choice(a)
+
+	return 0
