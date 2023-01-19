2ec968a8b84dab5c9e4c008aae6c239de3c8bd76
nanana-3 <94042880+nanana-3@users.noreply.github.com>
2023-01-19 15:49:13 +0900

    Create na-058.py

---
+++ na-058.py
@@ -0,0 +1,10 @@
+import random
+class AI(object):
+    def name(self):
+        return '22016058'
+    def play(self, board, color):
+        while True:
+            x = random.randint(0, board.N+1)
+            y = random.randint(0, board.N+1)
+            if board.put_and_reverse(x, y, color, reverse=False) > 0:
+                return (x, y)
